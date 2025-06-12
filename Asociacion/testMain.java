7ac0214df7293c260ba713092653ecfc1f953830
JGonzalez-cl <juanglezmendez3@gmail.com>
2025-06-12 15:11:17 +0200

    Asociacion

---
+++ Asociacion/testMain.java
@@ -0,0 +1,16 @@
+package Asociacion;
+
+public class testMain {
+
+    public static void main(String[] args) {
+
+        Departamento departamento = new Departamento("Pontevedra");
+
+        System.out.println("El censo del Departamento es " + departamento.getNombre() + " es "
+                + departamento.calcularCensoPoblacionDepartamento());
+        System.out.println("Municipios con poblacion mayor a 50.000 ");
+        departamento.buscarMunicipioConPoblacionMayor(500000);
+
+    }
+
+}
